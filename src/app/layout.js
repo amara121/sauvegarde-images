71cbdd4af17a64dff813f3bcebb5dcb349884579
amara121 <amaraff100@gmail.com>
2024-07-21 09:31:58 +0000

    add analytics vercel

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="fr">
       <body className={inter.className}>
         {children}
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
