eed380e6223811edf26cd93f20166b3a40e0f8bd
amara121 <amaraff100@gmail.com>
2024-07-25 00:20:47 +0000

    bug du signup et login terminer

--- src/app/(dashboard)/layout.jsx
+++ src/app/(dashboard)/layout.jsx
@@ -1,12 +1,26 @@
-import { createClient } from '@/utils/supabase/server'
+import Header from "@/components/header-component/Header";
+import Menu from "@/components/menu-component/Menu";
 
 const layout = async ({ children }) => {
-
   return (
-    <div>
-      {children}
-    </div>
-  )
-}
+    <div className="flex flex-col">
+      {/* le header */}
+      <div className="w-full h-20">
+        <Header />
+      </div>
 
-export default layout
+      {/* menu & content */}
+      <div className="grid grid-cols-[150px_minmax(320px,_1fr)] max-h-[calc(100vh_-_80px)] overflow-y-scroll">
+        {/* menu */}
+        <div className="relative">
+          <Menu />
+        </div>
+
+        {/* content */}
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default layout;
